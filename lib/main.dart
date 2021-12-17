08db5cff65b2b64dcc07969becaf2d9655d96f0b
AlienegraGeek <13629402950@163.com>
2021-12-17 20:45:08 +0800

    kraken

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,99 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:miniapp_demo/render/page/page_view.dart';
+import 'package:kraken/kraken.dart';
+import 'package:kraken_websocket/kraken_websocket.dart';
+import 'package:kraken_devtools/kraken_devtools.dart';
+import 'dart:ui';
 
 void main() {
-  runApp(const MyApp());
+  KrakenWebsocket.initialize();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // rootBundle.loadString('assets/page/test1.html').then((value) {
-    //   return Render.pageXml(value);
-    // });
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
+      title: 'Kraken Browser',
+      // theme: ThemeData.dark(),
+      home: MyBrowser(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class MyBrowser extends StatefulWidget {
+  MyBrowser({Key? key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String? title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-   Widget _widget = const Center();
+class _MyHomePageState extends State<MyBrowser> {
 
-  @override
-  void initState() {
-    rootBundle.loadString('assets/page/test1.html').then((value) {
-        setState(() {
-          _widget = Render.pageXml(value);
-        });
-    });
-    super.initState();
-  }
+  OutlineInputBorder outlineBorder = OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.transparent, width: 0.0),
+    borderRadius: const BorderRadius.all(
+      Radius.circular(20.0),
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _widget,
+    final MediaQueryData queryData = MediaQuery.of(context);
+    final TextEditingController textEditingController = TextEditingController();
+
+    Kraken? _kraken;
+    AppBar appBar = AppBar(
+      backgroundColor: Colors.black87,
+      titleSpacing: 10.0,
+      title: Container(
+        height: 40.0,
+        child: TextField(
+          controller: textEditingController,
+          onSubmitted: (value) {
+            textEditingController.text = value;
+            _kraken?.loadURL(value);
+          },
+          decoration: InputDecoration(
+            hintText: 'Enter a app url',
+            hintStyle: TextStyle(color: Colors.black54, fontSize: 16.0),
+            contentPadding: const EdgeInsets.all(10.0),
+            filled: true,
+            fillColor: Colors.grey,
+            border: outlineBorder,
+            focusedBorder: outlineBorder,
+            enabledBorder: outlineBorder,
+          ),
+          style: TextStyle(color: Colors.black, fontSize: 16.0),
+        ),
+      ),
+      // Here we take the value from the MyHomePage object that was created by
+      // the App.build method, and use it to set our appbar title.
     );
+
+    final Size viewportSize = queryData.size;
+    return Scaffold(
+        appBar: appBar,
+        body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: _kraken = Kraken(
+            devToolsService: ChromeDevToolsService(),
+            viewportWidth: viewportSize.width - queryData.padding.horizontal,
+            viewportHeight: viewportSize.height - appBar.preferredSize.height - queryData.padding.vertical,
+            bundleURL: 'assets/bundle.js',
+          ),
+        ));
   }
 }
