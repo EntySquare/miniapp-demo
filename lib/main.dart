886024fae239212c926f4650f25f8c526d283ecc
AlienegraGeek <13629402950@163.com>
2021-12-20 15:59:06 +0800

    kraken integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:kraken/kraken.dart';
+// import 'package:kraken_animation_player/kraken_animation_player.dart';
 
 void main() {
+  // KrakenAnimationPlayer.initialize();
   runApp(MyApp());
 }
 
@@ -10,8 +12,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Kraken kraken = Kraken(bundleURL: 'http://kraken.oss-cn-hangzhou.aliyuncs.com/demo/guide-styles.js');
-    // Kraken kraken = Kraken(background:Colors.white,bundlePath: 'assets/script/test.js');
-    Kraken kraken = Kraken(background: Colors.white, bundlePath: 'assets/page/kraken_test.html');
+
+    //js
+    // Kraken kraken = Kraken(
+    //   background: Colors.white,
+    //   bundlePath: 'assets/script/kraken_animation_demo.js',
+    // );
+
+    //version 0.9.0
+    // Kraken kraken = Kraken(background: Colors.white, bundlePath: 'assets/page/kraken_test.html');
+
+    //version 0.10.0
+    Kraken kraken = Kraken(background: Colors.white, bundle: KrakenBundle.fromUrl('assets://page/kraken_test.html'));
 
     return MaterialApp(
       title: 'Flutter Demo',
