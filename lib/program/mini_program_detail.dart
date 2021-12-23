cb6600c6fe5e347287aeeaa0871226b49b7c31e3
terrill <terilscaub@gmail.com>
2021-12-23 10:38:07 +0800

    commit issue

--- lib/program/mini_program_detail.dart
+++ lib/program/mini_program_detail.dart
@@ -13,42 +13,89 @@ class MiniProgramDetail extends StatefulWidget {
 }
 
 class _MiniProgramDetailState extends State<MiniProgramDetail> {
-  Kraken kraken = Kraken(viewportHeight: (window.physicalSize.height / window.devicePixelRatio) *0.88, background: Colors.black, bundle: KrakenBundle.fromUrl('assets://assets/script/app.js'));
+  Kraken kraken = Kraken(
+      viewportHeight:
+          (window.physicalSize.height / window.devicePixelRatio),
+      background: Colors.black,
+      bundle: KrakenBundle.fromUrl('assets://assets/script/app.js'));
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        // title: Text('耐克',style: TextStyle(color:Colors.white),),
-        centerTitle: true,
-        backgroundColor: Colors.black,
-        actions: [
-          IconButton(
-              iconSize: 19,
-              padding: EdgeInsets.zero,
-              splashRadius: 19,
-              icon: Icon(
-                Icons.more_horiz,
-                color: Colors.white,
-              ),
-              tooltip: 'More',
-              onPressed: () {}),
-          IconButton(
-              iconSize: 19,
-              splashRadius: 19,
-              padding: EdgeInsets.zero,
-              icon: Icon(
-                Icons.radio_button_checked,
-                color: Colors.white,
-              ),
-              tooltip: 'More',
-              onPressed: () {
-                Navigator.pop(context);
-              }),
+    // return Scaffold(
+    //   backgroundColor: Colors.black,
+    //   appBar: AppBar(
+    //     // title: Text('耐克',style: TextStyle(color:Colors.white),),
+    //     centerTitle: true,
+    //     backgroundColor: Colors.black,
+    //     actions: [
+    //       IconButton(
+    //           iconSize: 19,
+    //           padding: EdgeInsets.zero,
+    //           splashRadius: 19,
+    //           icon: Icon(
+    //             Icons.more_horiz,
+    //             color: Colors.white,
+    //           ),
+    //           tooltip: 'More',
+    //           onPressed: () {}),
+    //       IconButton(
+    //           iconSize: 19,
+    //           splashRadius: 19,
+    //           padding: EdgeInsets.zero,
+    //           icon: Icon(
+    //             Icons.radio_button_checked,
+    //             color: Colors.white,
+    //           ),
+    //           tooltip: 'More',
+    //           onPressed: () {
+    //             Navigator.pop(context);
+    //           }),
+    //     ],
+    //   ),
+    //   body: kraken
+    // );
+
+    return Material(
+      color: Colors.black,
+      child: Stack(
+        alignment: Alignment.topLeft,
+        clipBehavior: Clip.none,
+        children: [
+          kraken,
+          Positioned(
+            top: 30,
+            right: 10,
+              child: Container(
+                  alignment: Alignment.topLeft,
+                  padding: EdgeInsets.only(left: 30),
+                  child: Row(
+                    children: [
+                      IconButton(
+                          iconSize: 19,
+                          padding: EdgeInsets.zero,
+                          splashRadius: 19,
+                          icon: Icon(
+                            Icons.more_horiz,
+                            color: Colors.white,
+                          ),
+                          tooltip: 'More',
+                          onPressed: () {}),
+                      IconButton(
+                          iconSize: 19,
+                          splashRadius: 19,
+                          padding: EdgeInsets.zero,
+                          icon: Icon(
+                            Icons.radio_button_checked,
+                            color: Colors.white,
+                          ),
+                          tooltip: 'More',
+                          onPressed: () {
+                            Navigator.pop(context);
+                          }),
+                    ],
+                  ))),
         ],
       ),
-      body: kraken
-
     );
   }
 }
